4d7b44e3010f6645c8d2278fb71adda155deec8d
Milk <145270243+Milky47Way@users.noreply.github.com>
2024-03-24 17:08:37 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from pygame import *
+
+win_width = 700
+win_height = 500
+display.set_caption('Shooter')
+window = display.set_mode((win_width, win_height))
+
+img_back = 'b4731cf47a193ff46fa7ef82b637d7fa.jpg'
+img_hero = '7'
+
+#mixer.init()
+#mixer.music.load('space.ogg') #music
+#mixer.music.play()
+#fire_sound = mixer.Sound('fire.ogg')
+
+background = transform.scale(image.load(img_back), (win_width, win_height))
+finish = False
+run = True
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+    if not finish:
+        window.blit(background, (0,0))
+        display.update()
+    time.delay(50)
+
+#https://github.com/AnnaSepita/shooter_photo
